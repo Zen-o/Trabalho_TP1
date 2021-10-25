47ae9253e35ed4fb758683cc35630c6a846c5914
Gabriel Barbosa Gomes <zenoreude@gmail.com>
2021-10-25 20:16:47 -0300

    adicionei as telas atestado,encaminhamento,receita, além da classe dados

--- src/Screens/Medico.java
+++ src/Screens/Medico.java
@@ -16,7 +16,7 @@ public class Medico extends javax.swing.JFrame {
      */
     public Medico() {
         initComponents();
-        this.setExtendedState(MAXIMIZED_BOTH);
+        this.setExtendedState(MAXIMIZED_BOTH);    
     }
 
     /**
@@ -42,10 +42,25 @@ public class Medico extends javax.swing.JFrame {
         setIconImage(new javax.swing.ImageIcon(getClass().getResource("/icons/icon_window.png")).getImage());
 
         jButton1.setText("Atestado");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
 
         jButton2.setText("Receitas");
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton2ActionPerformed(evt);
+            }
+        });
 
         jButton3.setText("Encaminhamento");
+        jButton3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton3ActionPerformed(evt);
+            }
+        });
 
         jPanel1.setBorder(javax.swing.BorderFactory.createTitledBorder("Dados triagem"));
 
@@ -163,6 +178,20 @@ public class Medico extends javax.swing.JFrame {
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        new Atestado().setVisible(true);
+    }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+        // TODO add your handling code here:
+        new Receita().setVisible(true);
+    }//GEN-LAST:event_jButton2ActionPerformed
+
+    private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
+        // TODO add your handling code here:
+        new Encaminhamento().setVisible(true);
+    }//GEN-LAST:event_jButton3ActionPerformed
 
     /**
      * @param args the command line arguments
---
+++ src/entities/Dados.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entities;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Gabriel
+ */
+public class Dados {
+    private static ArrayList<Paciente> listaPacientes;
+    private static ArrayList<Recepcionista> listaRecepcionistas;
+    private static ArrayList<Medico> listaMedicos;
+
+    public static ArrayList<Paciente> getListaPacientes() {
+        return listaPacientes;
+    }
+
+    public static void adicionarPaciente(Paciente paciente) {
+        Dados.listaPacientes.add(paciente);
+    }
+
+    public static ArrayList<Recepcionista> getListaRecepcionistas() {
+        return listaRecepcionistas;
+    }
+
+    public static void adicionarRecepcionista(Recepcionista recepcionista) {
+        Dados.listaRecepcionistas.add(recepcionista);
+    }
+
+    public static ArrayList<Medico> getListaMedicos() {
+        return listaMedicos;
+    }
+
+    public static void adicionarMedico(Medico medico) {
+        Dados.listaMedicos.add(medico);
+    }  
+}
